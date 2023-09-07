8a1f0018efa0ca7d434734ea24e615338092b40f
alyssa32 <alyssa.durdey@gmail.com>
2023-09-07 15:59:21 -0400

    Created an orange canvas for workflow experiment

--- experiments/workflow/js/script.js
+++ experiments/workflow/js/script.js
@@ -1,8 +1,8 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
@@ -10,23 +10,18 @@
 
 /**
  * Description of preload
-*/
-function preload() {
-
-}
-
+ */
+function preload() {}
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
-
+  createCanvas(480, 480);
+  background(255, 100, 10);
 }
-
 
 /**
  * Description of draw()
-*/
-function draw() {
-
-}
+ */
+function draw() {}
