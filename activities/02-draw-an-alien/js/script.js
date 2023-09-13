65f776cc8e13d7cb4fcb457f85ba45a9c7f7deb1
alyssa32 <alyssa.durdey@gmail.com>
2023-09-13 16:19:16 -0400

    Activity 02: Started the draw an alien activity

---
+++ activities/02-draw-an-alien/js/script.js
@@ -0,0 +1,28 @@
+/**
+Drawing Experiment
+ Alyssa Durdey
+ *
+ Experimenting with drawing shapes and colours using p5's functions
+ */
+
+"use strict";
+
+/**
+ Does nothing
+ */
+function preload() {}
+
+/**
+ Draws the alien
+ */
+function setup() {
+  createCanvas(500, 500);
+  background(200, 200, 200);
+  rectMode(CENTER);
+  ellipse(250, 250, 80, 80);
+}
+
+/**
+Does nothing
+ */
+function draw() {}
