dbfa5bce54f24397cc3f906f2e8f14d69eada5d0
alyssa32 <alyssa.durdey@gmail.com>
2023-09-30 03:52:27 -0400

    03: Randomized the Food

--- activities/03-dodge-em/js/script.js
+++ activities/03-dodge-em/js/script.js
@@ -8,12 +8,28 @@
 
 "use strict";
 let food = {
-    x: 500,
+    x: 50,
     y: 500,
-    size: 20,
+    size: 15,
     r: 255,
     g: 227,
     b: 115,
+}
+let pacman = {
+    x: 600,
+    y: 450,
+    size: 60,
+    r: 252,
+    g: 186,
+    b: 3,
+}
+let box = {
+    x: 500,
+    y: 500,
+    size: 100,
+    r: 5,
+    g: 68,
+    b: 255,
 }
 
 /**
@@ -29,16 +45,30 @@ function preload() {
 */
 function setup() {
     createCanvas(1000,1000);
-background(0);
+ background(0);
+    //Draws the Pacman
+fill(pacman.r, pacman.g, pacman.b);
+ellipse(mouseX, mouseY, pacman.size);
+
+drawFood(25);
 }
 
-
+function drawFood(quantity){
+     fill(food.r, food.g, food.b);
+    for(var i = 0; i < quantity; i++){
+ellipse(random(width), random(height), food.size);
+}
+}
 /**
  * Description of draw()
 */
 function draw() {
+// Draws the Background
+
 // Draws the food nuggets
 fill(food.r, food.g, food.b);
-ellipse (food.x, food.y, food.size);
+
+
+
 
 }
