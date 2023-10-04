6d3a0d99a33acc4902e333c1dcca6153fe24a0f7
alyssa32 <alyssa.durdey@gmail.com>
2023-10-04 13:16:33 -0400

    E3: Placed the Obstacles and Food
    I started to re-align the food. So far used one loop. The obstacles are placed and coloured.

--- activities/03-dodge-em/js/script.js
+++ activities/03-dodge-em/js/script.js
@@ -8,8 +8,8 @@
 
 "use strict";
 let food = {
-    x: 50,
-    y: 500,
+    x: 80,
+    y: 470,
     size: 15,
     r: 255,
     g: 227,
@@ -23,15 +23,45 @@ let pacman = {
     g: 186,
     b: 3,
 }
-let box = {
-    x: 500,
-    y: 500,
-    size: 100,
+let boxOne = {
+    x: 0,
+    y: 180,
+    w: 200,
+    h: 50,
     r: 5,
     g: 68,
     b: 255,
 }
 
+let boxTwo = {
+    x: 100,
+    y: 360,
+    w: 400,
+    h: 50,
+    r: 5,
+    g: 68,
+    b: 255,
+}
+
+let boxThree = {
+    x: 700,
+    y: 540,
+    w: 300,
+    h: 50,
+    r: 5,
+    g: 68,
+    b: 255,
+}
+
+let boxFour = {
+    x: 300,
+    y: 720,
+    w: 300,
+    h: 50,
+    r: 5,
+    g: 68,
+    b: 255,
+}
 /**
  * Description of preload
 */
@@ -45,30 +75,46 @@ function preload() {
 */
 function setup() {
     createCanvas(1000,1000);
- background(0);
-    //Draws the Pacman
-fill(pacman.r, pacman.g, pacman.b);
-ellipse(mouseX, mouseY, pacman.size);
-
-drawFood(25);
-}
-
-function drawFood(quantity){
-     fill(food.r, food.g, food.b);
-    for(var i = 0; i < quantity; i++){
-ellipse(random(width), random(height), food.size);
-}
 }
 /**
  * Description of draw()
 */
 function draw() {
-// Draws the Background
+       // Draws the Black Background
+ background(0);
 
-// Draws the food nuggets
+    //Draws the Food Nuggets
+    noStroke();
 fill(food.r, food.g, food.b);
 
+     let x = food.x
+     let numSegments = 13;
+     let segmentsDrawn = 0;
 
+     while (segmentsDrawn < numSegments) {
+ellipse(x, food.y, food.size);
+x = x + 70;
+segmentsDrawn = segmentsDrawn + 1;
+     }
 
+  //Draws the Pacman
+fill(pacman.r, pacman.g, pacman.b);
+ellipse(mouseX, mouseY, pacman.size);
 
-}
+  //Draws the First Box
+  stroke(boxOne.r, boxOne.g, boxOne.b);
+  strokeWeight(4);
+  fill(0);
+  rect(boxOne.x, boxOne.y, boxOne.w, boxOne.h);
+
+   //Draws the Second Box
+  rect(boxTwo.x, boxTwo.y, boxTwo.w, boxTwo.h);
+
+   //Draws the Third Box
+  rect(boxThree.x, boxThree.y, boxThree.w, boxThree.h);
+
+   //Draws the Fourth Box
+  rect(boxFour.x, boxFour.y, boxFour.w, boxFour.h);
+     }
+    
+
