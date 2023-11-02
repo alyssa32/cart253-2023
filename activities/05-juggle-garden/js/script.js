a7d6c59bbdb6caa15ed3f587bc1ac5df4fc499c8
alyssa32 <alyssa.durdey@gmail.com>
2023-11-01 23:25:46 -0400

    05: Preloaded All Images

--- activities/05-juggle-garden/js/script.js
+++ activities/05-juggle-garden/js/script.js
@@ -1,32 +1,35 @@
 /**
  * Title of Project
  * Author Name
- * 
- * This is a template. You must fill in the title, author, 
+ *
+ * This is a template. You must fill in the title, author,
  * and this description to match your project!
  */
 
 "use strict";
+let = redTulipImg;
+let = blueTulipImg;
+let = yellowTulipImg;
+let = stemImg;
+let = pixelBeeImg;
 
 /**
  * Description of preload
-*/
+ */
 function preload() {
-
+  redTulipImg = loadImage("assets/images/redTulip.png");
+  blueTulipImg = loadImage("assets/images/blueTulip.png");
+  yellowTulipImg = loadImage("assets/images/yellowTulip.png");
+  stemImg = loadImage("assets/images/stem.png");
+  pixelBeeImg = loadImage("assets/images/pixelBee.png");
 }
-
 
 /**
  * Description of setup
-*/
-function setup() {
-
-}
-
+ */
+function setup() {}
 
 /**
  * Description of draw()
-*/
-function draw() {
-
-}
+ */
+function draw() {}
