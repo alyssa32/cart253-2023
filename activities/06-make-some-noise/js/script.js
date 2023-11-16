b26e7dec736a316204e24059c973a0205231a4c1
alyssa32 <alyssa.durdey@gmail.com>
2023-11-16 13:43:54 -0500

    06: Created a js File for the Scenes

--- activities/06-make-some-noise/js/script.js
+++ activities/06-make-some-noise/js/script.js
@@ -1,32 +1,29 @@
 /**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Make Some Noise
+ * Alyssa Durdey
+ *
+ * A program that allows the user to change between scenes using the amplitude of their voice.
  */
 
 "use strict";
 
+let canvasX = 800;
+let canvasY = 800;
+
 /**
  * Description of preload
-*/
-function preload() {
-
-}
-
+ */
+function preload() {}
 
 /**
  * Description of setup
-*/
+ */
 function setup() {
-
+  createCanvas(canvasX, canvasY);
+  fill;
 }
-
 
 /**
  * Description of draw()
-*/
-function draw() {
-
-}
+ */
+function draw() {}
