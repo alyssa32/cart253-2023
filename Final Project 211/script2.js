2c5b6c1b80a2b8c9e2495ff20cff7340f2a69394
alyssa32 <alyssa.durdey@gmail.com>
2023-12-10 17:16:16 -0500

    Added endscreens

--- Final Project 211/script2.js
+++ Final Project 211/script2.js
@@ -4,6 +4,11 @@ let verticalOffset = 0;
 let rightkey = false;
 let leftkey = false;
 
+let maxImagesToDisplay = 9;
+let imagesAlreadyGenerated = 0;
+
+let pluggIn = false;
+
 image_array = [
   "AiArt1.jpeg",
   "AiArt2.webp",
@@ -21,12 +26,21 @@ image_array = [
 ];
 
 function getRandomImage() {
-  //Get a random index
-  random_index = Math.floor(Math.random() * image_array.length);
-  //Get an image at the random index
-  selected_image = image_array[random_index];
-  //Display the image
-  document.getElementById("art").src = `./images/${selected_image}`;
+  if (imagesAlreadyGenerated < maxImagesToDisplay) {
+    //Get a random index
+    random_index = Math.floor(Math.random() * image_array.length);
+    //Get an image at the random index
+    selected_image = image_array[random_index];
+    //Display the image
+    document.getElementById("art").src = `./images/${selected_image}`;
+
+    imagesAlreadyGenerated++;
+
+    // Update the image counter
+    updateImageCounter();
+  } else {
+    displayWinScreen();
+  }
 }
 //----------------------------------- ROBOT MOVEMENT ------------------------
 setInterval(robot, 20);
@@ -44,14 +58,17 @@ function keyUp(event) {
 }
 //Moves the robot from left to right if the left or right arrow is pressed
 function robot() {
+  // If the robot reaches the end of the screen stop it.
   if (rightkey == true) {
-    if (imageOffset >= window.screen.width - 140) {
+    if (imageOffset >= window.screen.width - 240) {
     } else {
       imageOffset = imageOffset + 3;
     }
   }
   if (leftkey == true) {
-    if (imageOffset >= window.screen.width + 140) {
+    //Constrict to robot to the left side of the screen
+    if (imageOffset <= 0) {
+      //If the robot is already at the left side of the screen, do nothing
     } else {
       imageOffset = imageOffset - 3;
     }
@@ -61,8 +78,72 @@ function robot() {
 }
 //----------------------------------- TV ------------------------
 function plugTV() {
-  //When the image is clicked, the TV plugs into the socket (switches between images)
-  document.getElementById("plug").src = "images/plugged.png";
-  //document.getElementById("art").src = "images/AiArt2.webp";
-  getRandomImage();
+  if (pluggIn == false) {
+    //When the image is clicked, the TV plugs into the socket (switches between images)
+    document.getElementById("plug").src = "images/plugged.png";
+    //document.getElementById("art").src = "images/AiArt2.webp";
+    getRandomImage();
+
+    document.getElementById("ai").style.display = "block";
+    document.getElementById("human").style.display = "block";
+    document.getElementById("art").style.display = "block";
+
+    pluggIn = true;
+  }
+}
+
+// Check if the image is AI or not, based on the file name
+function checkIfImageIsAI() {
+  let imageName = document.getElementById("art").src;
+  // imageName returns: "http://127.0.0.1:5500/images/humanMadeArt3.jpg"
+
+  imageName = imageName.split("/");
+
+  // imageName returns: ["http:", "" , "127.0.0.1:5500", "images", "[image source]"]
+
+  // We want to get only the image source from the array
+
+  imageName = imageName[imageName.length - 1];
+
+  // Now we have the name of the source file, and we want to get the prefix of either Ai... or human...
+  let prefix = imageName.substring(0, 2);
+
+  if (prefix == "Ai") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function buttonAIArt() {
+  if (checkIfImageIsAI() == true) {
+    console.log("YES! It's AI art!");
+
+    getRandomImage();
+  } else {
+    displayLoseScreen();
+  }
+}
+
+function buttonHumanArt() {
+  if (checkIfImageIsAI() == false) {
+    console.log("No! It's human art!");
+
+    getRandomImage();
+  } else {
+    displayLoseScreen();
+  }
+}
+
+function updateImageCounter() {
+  document.getElementById("imgCounter").innerHTML =
+    "Image: " + imagesAlreadyGenerated + "/" + maxImagesToDisplay;
+}
+
+function displayLoseScreen() {
+  alert("You made the wrong decision.\nGAME OVER!");
+  document.getElementById("lose").src = "lose.html";
+}
+function displayWinScreen() {
+  document.getElementById("win").src = "win.html";
 }
